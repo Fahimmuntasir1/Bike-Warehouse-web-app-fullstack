6b68d59440dd40d9cdf2a76f5d79dcd1ea31791b
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-04-30 15:30:23 +0600

    setup and first commit

---
+++ src/components/Pages/InventoryItems/InventoryItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InventoryItems = () => {
+    return (
+        <div>
+            <h2>Inventory</h2>
+        </div>
+    );
+};
+
+export default InventoryItems;
