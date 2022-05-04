5081a2b9cf62a4568156d8e42580be905d202078
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-04 12:52:23 +0600

    contact form added

--- src/components/Pages/Contact/Contact .js
+++ src/components/Pages/Contact/Contact .js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact  = () => {
-    return (
+const Contact = () => {
+  return (
+    <div className="bg-light">
+      <h2 className="text-center py-5">Contact Us</h2>
+
+      <div className="flex justify-content-around align-items-center">
         <div>
-            <h2>Contact Us</h2>
+          <lottie-player
+            src="https://assets4.lottiefiles.com/packages/lf20_u25cckyh.json"
+            background="transparent"
+            speed="1"
+            style={{ width: "400px", height: "400px" }}
+            loop
+            autoplay
+          ></lottie-player>
         </div>
-    );
+        <div className="contact-div">
+          <form className="flex contact-form">
+            <input type="text" placeholder="Enter your name" />
+            <input type="email" name="" id="" placeholder="Your email" />
+            <textarea
+              name="messages"
+              id=""
+              cols="20"
+              rows="7 "
+              placeholder="Your message here"
+            ></textarea>
+            <button>SEND</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Contact ;
+export default Contact;
